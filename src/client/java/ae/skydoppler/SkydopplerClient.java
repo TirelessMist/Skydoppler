8559d9e90c19b13fb2001f388b4fb3daea856f48
TirelessMist <emardag@icloud.com>
2025-04-15 15:55:02 -0500

    Initial commit

---
+++ src/client/java/ae/skydoppler/SkydopplerClient.java
@@ -0,0 +1,10 @@
+package ae.skydoppler;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkydopplerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
