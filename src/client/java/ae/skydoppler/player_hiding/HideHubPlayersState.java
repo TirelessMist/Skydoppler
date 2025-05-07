e58b26e5ac6da44e78b4dc75fb2b42e7a2946e45
TirelessMist <emardag@icloud.com>
2025-05-07 18:04:05 -0500

    Moved the logic to determine whether players should be hidden in the location the player is in to this file (HideHubPlayersState.java) to be more centralized and accessible.

--- src/client/java/ae/skydoppler/player_hiding/HideHubPlayersState.java
+++ src/client/java/ae/skydoppler/player_hiding/HideHubPlayersState.java
@@ -1,10 +1,32 @@
 package ae.skydoppler.player_hiding;
 
+import ae.skydoppler.SkydopplerClient;
+import ae.skydoppler.skyblock_locations.SkyblockIslandEnum;
+
 public class HideHubPlayersState {
-    private enum HideLocationMode {
+    public enum HideLocationMode {
         VILLAGE_ONLY, HUB_ENTIRE, DUNGEON_AND_NORMAL_HUB, DUNGEON_AND_VILLAGE
     }
 
+    public static HideLocationMode hideLocationMode = HideLocationMode.DUNGEON_AND_NORMAL_HUB;
     public static double showRange = 12.0 /*0.1 * MinecraftClient.getInstance().worldRenderer.getViewDistance()*/;
+
+    public static boolean shouldHidePlayers() {
+        switch (HideHubPlayersState.hideLocationMode) {
+            case HideHubPlayersState.HideLocationMode.HUB_ENTIRE -> {
+                return SkydopplerClient.currentIsland == SkyblockIslandEnum.HUB;
+            }
+            case HideHubPlayersState.HideLocationMode.DUNGEON_AND_NORMAL_HUB -> {
+                return SkydopplerClient.currentIsland == SkyblockIslandEnum.DUNGEON_HUB || SkydopplerClient.currentIsland == SkyblockIslandEnum.HUB;
+            }
+            case HideHubPlayersState.HideLocationMode.VILLAGE_ONLY -> {
+                return SkydopplerClient.currentIsland == SkyblockIslandEnum.HUB && SkydopplerClient.currentZone == SkyblockIslandEnum.HubZones.VILLAGE;
+            }
+            case HideHubPlayersState.HideLocationMode.DUNGEON_AND_VILLAGE -> {
+                return SkydopplerClient.currentIsland == SkyblockIslandEnum.DUNGEON_HUB || (SkydopplerClient.currentIsland == SkyblockIslandEnum.HUB && SkydopplerClient.currentZone == SkyblockIslandEnum.HubZones.VILLAGE);
+            }
+        }
+        return false;
+    }
 
 }
