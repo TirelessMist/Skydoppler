3fcaa8747fca58614f8d63d64656669057dd89a4
TirelessMist <emardag@icloud.com>
2025-05-07 15:26:04 -0500

    Added all the zones' literal names (as they appear on the scoreboard in the English language on Hypixel) into the different' island's data.

--- src/client/java/ae/skydoppler/skyblock_locations/SkyblockIslandEnum.java
+++ src/client/java/ae/skydoppler/skyblock_locations/SkyblockIslandEnum.java
@@ -1,75 +1,389 @@
 package ae.skydoppler.skyblock_locations;
 
-import java.util.Collection;
-import java.util.List;
-
 public enum SkyblockIslandEnum {
-    // These are the different islands in Hypixel Skyblock
+    NONE(SkyblockIslandType.NONE, null),
 
-    NONE("None", SkyblockIslandType.NONE, List.of("None")),
+    PRIVATE_ISLAND(SkyblockIslandType.NONE, null),
 
-    PRIVATE_ISLAND("Private Island", SkyblockIslandType.NONE, List.of("Your Island")),
+    HUB(SkyblockIslandType.HUB, HubZones.class),
 
-    HUB("Hub", SkyblockIslandType.HUB, List.of("Archery Range", "Artist's Abode", "Auction House", "Bank",
-            "Bazaar Alley", "Blacksmith", "Builder's House", "Canvas Room",
-            "Coal Mine", "Colosseum", "Community Center", "Election Room",
-            "Farm", "Farmhouse", "Fashion Shop", "Fisherman's Hut",
-            "Flower House", "Thaumaturgist", "Village", "Weaponsmith", "Wilderness", "Wizard Tower", "Fishing Outpost")),
+    DUNGEON_HUB(SkyblockIslandType.HUB, null),
 
-    DUNGEON_HUB("Dungeon Hub", SkyblockIslandType.HUB, List.of("Dungeon Hub")),
-    DUNGEON("Dungeon", SkyblockIslandType.DUNGEON_ISLAND, List.of("Dungeon", "The Catacombs")),
+    CRIMSON_ISLE(SkyblockIslandType.COMBAT_ISLAND, null),
+    SPIDER_DEN(SkyblockIslandType.COMBAT_ISLAND, null),
+    THE_END(SkyblockIslandType.COMBAT_ISLAND, TheEndZones.class),
 
-    CRIMSON_ISLE("Crimson Isle", SkyblockIslandType.COMBAT_ISLAND, List.of("Crimson Isle", "Stronghold", "Blazing Volcano", "Scarleton", "Mystic Marsh", "Dragontail", "Burning Desert", "Phlegblast Pool")),
-    SPIDER_DEN("Spider's Den", SkyblockIslandType.COMBAT_ISLAND, List.of("Spider Den")),
-    THE_END("The End", SkyblockIslandType.COMBAT_ISLAND, List.of("The End", "Dragon's Nest", "Zealot Bruiser Hideout", "End Slate")),
+    GOLD_MINE(SkyblockIslandType.MINING_ISLAND, null),
+    DEEP_CAVERNS(SkyblockIslandType.MINING_ISLAND, null),
+    DWARVEN_MINES(SkyblockIslandType.MINING_ISLAND, null),
+    CRYSTAL_HOLLOWS(SkyblockIslandType.MINING_ISLAND, null),
+    GLACITE_TUNNELS(SkyblockIslandType.MINING_ISLAND, null),
 
-    GOLD_MINE("Gold Mine", SkyblockIslandType.MINING_ISLAND, List.of("Gold Mine")),
-    DEEP_CAVERNS("Deep Caverns", SkyblockIslandType.MINING_ISLAND, List.of("Deep Caverns", "Lapis Quarry", "Gold Mine", "Diamond Mine", "Emerald Mine", "Obsidian Mine")),
-    DWARVEN_MINES("Dwarven Mines", SkyblockIslandType.MINING_ISLAND, List.of("Dwarven Mines")),
-    CRYSTAL_HOLLOWS("Crystal Hollows", SkyblockIslandType.MINING_ISLAND, List.of("Crystal Hollows", "Crystal Nucleus", "Goblin Holdout", "Magma Fields", "Jungle Temple", "Mines of Divan", "Jungle", "Precursor Remnants", "Mithril Deposits", "Fairy Grotto")),
-    GLACITE_TUNNELS("Glacite Tunnels", SkyblockIslandType.MINING_ISLAND, List.of("Glacite Mineshaft", "Glacite Tunnels")),
+    JERRY_ISLAND(SkyblockIslandType.NONE, null),
 
-    JERRY_ISLAND("Jerry Island", SkyblockIslandType.NONE, List.of("Jerry's Workshop", "Glacial Cave", "Hot Springs")),
+    BACKWATER_BAYOU(SkyblockIslandType.FISHING_ISLAND, null),
 
-    BACKWATER_BAYOU("Backwater Bayou", SkyblockIslandType.FISHING_ISLAND, List.of("Backwater Bayou")),
+    THE_PARK(SkyblockIslandType.FORAGING_ISLAND, null),
 
-    THE_PARK("The Park", SkyblockIslandType.FORAGING_ISLAND, List.of("The Park")),
+    THE_GARDEN(SkyblockIslandType.FARMING_ISLAND, null),
+    THE_BARN(SkyblockIslandType.FARMING_ISLAND, null),
+    MUSHROOM_DESERT(SkyblockIslandType.FARMING_ISLAND, null),
 
-    THE_GARDEN("The Garden", SkyblockIslandType.FARMING_ISLAND, List.of("The Garden")),
-    THE_BARN("The Barn", SkyblockIslandType.FARMING_ISLAND, List.of("The Barn")),
-    MUSHROOM_DESERT("Mushroom Desert", SkyblockIslandType.FARMING_ISLAND, List.of("Mushroom Desert")),
+    THE_RIFT(SkyblockIslandType.RIFT_ISLAND, null);
 
-    THE_RIFT("The Rift", SkyblockIslandType.RIFT_ISLAND, List.of("The Rift", "Mountaintop"));
-
-
-    private final String name;
     private final SkyblockIslandType islandType;
-    private final List<String> literalNames; // the name that is used on the scoreboard, case-sensitive (e.g. "The Park")
+    private final Class<? extends Enum<?>> zones;
 
-    SkyblockIslandEnum(String name, SkyblockIslandType islandType, List<String> literalNames) {
-        this.name = name;
+    SkyblockIslandEnum(SkyblockIslandType islandType, Class<? extends Enum<?>> zones) {
         this.islandType = islandType;
-        this.literalNames = literalNames;
-    }
-
-    public static boolean isValidLocation(String location) {
-        for (SkyblockIslandEnum loc : SkyblockIslandEnum.values()) {
-            if (loc.name.equalsIgnoreCase(location)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public String getName() {
-        return name;
+        this.zones = zones;
     }
 
     public SkyblockIslandType getIslandType() {
         return islandType;
     }
 
-    public List<String> getLiteralNames() {
-        return literalNames;
+    public Class<? extends Enum<?>> getZones() {
+        return zones;
     }
+
+    public enum HubZones {
+        HUB("hub"),
+        VILLAGE("village"),
+        BAZAAR("bazaar alley"),
+        AUCTION_HOUSE("auction house"),
+        BANK("bank"),
+        COMMUNITY_CENTER("community center"),
+        ELECTION_ROOM("election room"),
+        MUSEUM("museum"),
+        ARCHERY_RANGE("archery range"),
+        ARTIST_ABODE("artist's abode"),
+        BLACKSMITH("blacksmith"),
+        BUILDER_HOUSE("builder's house"),
+        CANVAS_ROOM("canvas room"),
+        COAL_MINE("coal mine"),
+        COLOSSEUM("colosseum"),
+        FARM("farm"),
+        FARMHOUSE("farmhouse"),
+        FASHION_SHOP("fashion shop"),
+        FISHERMAN_HUT("fisherman's hut"),
+        FLOWER_HOUSE("flower house"),
+        FOREST("forest"),
+        GRAVEYARD("graveyard"),
+        CATACOMBS_ENTRANCE("catacombs entrance"),
+        HEXATORUM("hexatorum"),
+        UNINCORPORATED("unincorporated"),
+        LIBRARY("library"),
+        MOUNTAIN("mountain"),
+        PET_CARE("pet care"),
+        RABBIT_HOUSE("rabbit house"),
+        RUINS("ruins"),
+        SHEN_AUCTION("shen's auction"),
+        TAVERN("tavern"),
+        THAUMATURGIST("thaumaturgist"),
+        WEAPONSMITH("weaponsmith"),
+        WILDERNESS("wilderness"),
+        WIZARD_TOWER("wizard tower"),
+        FISHING_OUTPOST("fishing outpost");
+
+        private final String name;
+        HubZones(String name) {
+            this.name = name;
+        }
+    }
+    public enum BarnZones {
+        BARN("barn"),
+        WINDMILL("windmill");
+
+        private final String name;
+        BarnZones(String name) {
+            this.name = name;
+        }
+    }
+    public enum MushroomDesertZones {
+        MUSHROOM_DESERT("mushroom desert"),
+        DESERT_SETTLEMENT("desert settlement"),
+        GLOWING_MUSHROOM_CAVE("glowing mushroom cave"),
+        JAKE_HOUSE("jake's house"),
+        MUSHROOM_GORGE("mushroom gorge"),
+        OASIS("oasis"),
+        OVERGROWN_MUSHROOM_CAVE("overgrown mushroom cave"),
+        SHEPHERD_KEEP("shepherd's keep"),
+        TRAPPER_DEN("trapper's den"),
+        TREASURE_HUNTER_CAMP("treasure hunter camp");
+
+        private final String name;
+        MushroomDesertZones(String name) {
+            this.name = name;
+        }
+    }
+    public enum TheParkZones {
+        THE_PARK("the park"),
+        BIRCH_PARK("birch park"),
+        HOWLING_CAVE("howling cave"),
+        SPRUCE_WOODS("spruce woods"),
+        LONELY_ISLAND("lonely island"),
+        VIKING_LONGHOUSE("viking longhouse"),
+        DARK_THICKET("dark thicket"),
+        SAVANNA_WOODLAND("savanna woodland"),
+        MELODY_PLATEAU("melody's plateau"),
+        JUNGLE_ISLAND("jungle island");
+
+        private final String name;
+        TheParkZones(String name) {
+            this.name = name;
+        }
+    }
+    public enum SpiderDenZones {
+        SPIDER_DEN("spider's den"),
+        SPIDER_MOUND("spider mound"),
+        GRAVEL_MINES("gravel mines"),
+        GRANDMA_HOUSE("grandma's house"),
+        ARACHNE_BURROW("arachne's burrow"),
+        ARACHNE_SANCTUARY("arachne's sanctuary"),
+        ARCHAEOLOGIST_CAMP("archaeologist's camp");
+
+        private final String name;
+        SpiderDenZones(String name) {
+            this.name = name;
+        }
+    }
+    public enum TheEndZones {
+        THE_END("the end"),
+        DRAGON_NEST("dragon's nest"),
+        VOID_SEPULTURE("void sepulchre"),
+        VOID_SLATE("void slate"),
+        ZEALOT_BRUISER_HIDEOUT("zealot bruiser hideout");
+
+        private final String name;
+        TheEndZones(String name) {
+            this.name = name;
+        }
+    }
+    public enum CrimsonIsleZones {
+        CRIMSON_ISLE("crimson isle"),
+        STRONGHOLD("stronghold"),
+        CRIMSON_FIELDS("crimson fields"),
+        BLAZING_VOLCANO("blazing volcano"),
+        ODGER_HUT("odger's hut"),
+        PLHLEGBLAST_POOL("plhlegblast pool"),
+        MAGMA_CHAMBER("magma chamber"),
+        AURA_LAB("aura's lab"),
+        MATRIARCH_LAIR("matriarch's lair"),
+        BELLY_OF_THE_BEAST("belly of the beast"),
+        DOJO("dojo"),
+        DOJO_ARENA("dojo arena"),
+        BURNING_DESERT("burning desert"),
+        MYSTIC_MARSH("mystic marsh"),
+        BARBARIAN_OUTPOST("barbarian outpost"),
+        MAGE_OUTPOST("mage outpost"),
+        DRAGONTAIL("dragontail"),
+        CHIEF_HUT("chief's hut"),
+        DRAGONTAIL_BLACKSMITH("dragontail blacksmith"),
+        DRAGONTAIL_TOWNSQUARE("dragontail townsquare"),
+        DRAGONTAIL_AUCTION_HOUSE("dragontail auction house"),
+        DRAGONTAIL_BAZAAR("dragontail bazaar"),
+        DRAGONTAIL_BANK("dragontail bank"),
+        MINION_SHOP("minion shop"),
+        THE_DUKEDOM("the dukedom"),
+        THE_BASTION("the bastion"),
+        SCARLETON("scarleton"),
+        COMMUNITY_CENTER("community center"),
+        THRONE_ROOM("throne room"),
+        MAGE_COUNCIL("mage council"),
+        SCARLETON_PLAZA("scarleton plaza"),
+        SCARLETON_MINION_SHOP("scarleton minion shop"),
+        SCARLETON_AUCTION_HOUSE("scarleton auction house"),
+        SCARLETON_BAZAAR("scarleton bazaar"),
+        SCARLETON_BANK("scarleton bank"),
+        SCARLETON_BLACKSMITH("scarleton blacksmith"),
+        IGRUPAN_HOUSE("igrupan's house"),
+        IGRUPAN_CHICKEN_COOP("igrupan's chicken coop"),
+        CATHEDRAL("cathedral"),
+        COURTYARD("courtyard"),
+        THE_WASTELAND("the wasteland"),
+        RUINS_OF_ASHFANG("ruins of ashfang"),
+        FORGOTTEN_SKULL("forgotten skull"),
+        SMOLDERING_TOMB("smoldering tomb");
+
+        private final String name;
+        CrimsonIsleZones(String name) {
+            this.name = name;
+        }
+    }
+    public enum GoldMineZones {
+        GOLD_MINE("gold mine");
+
+        private final String name;
+        GoldMineZones(String name) {
+            this.name = name;
+        }
+    }
+    public enum DeepCavernsZones {
+        DEEP_CAVERNS("deep caverns"),
+        GUNPOWDER_MINES("gunpowder mines"),
+        LAPIS_QUARRY("lapis quarry"),
+        PIGMEN_DEN("pigmen's den"),
+        SLIMEHILL("slimehill"),
+        DIAMOND_RESERVE("diamond reserve"),
+        OBSIDIAN_SANCTUARY("obsidian sanctuary");
+
+        private final String name;
+        DeepCavernsZones(String name) {
+            this.name = name;
+        }
+    }
+    public enum DwarvenMinesZones {
+        DWARVEN_MINES("dwarven mines"),
+        ARISTOCRAT_PASSAGE("aristocrat passage"),
+        BARRACKS_OF_HEROES("barracks of heroes"),
+        CC_MINECARTS_CO("c&c minecarts co."),
+        CLIFFSIDE_VEINS("cliffside veins"),
+        DIVAN_GATEWAY("divan's gateway"),
+        DWARVEN_TAVERN("dwarven tavern"),
+        DWARVEN_VILLAGE("dwarven village"),
+        FAR_RESERVE("far reserve"),
+        FORGE_BASIN("forge basin"),
+        GATES_TO_THE_MINES("gates to the mines"),
+        GOBLIN_BURROWS("goblin burrows"),
+        GRAND_LIBRARY("grand library"),
+        GREAT_ICE_WALL("great ice wall"),
+        HANGING_COURT("hanging court"),
+        LAVA_SPRINGS("lava springs"),
+        MINER_GUILD("miner's guild"),
+        PALACE_BRIDGE("palace bridge"),
+        RAMPART_QUARRY("rampart's quarry"),
+        ROYAL_MINES("royal mines"),
+        ROYAL_PALACE("royal palace"),
+        ROYAL_QUARTERS("royal quarters"),
+        THE_FORGE("the forge"),
+        THE_LIFT("the lift"),
+        THE_MIST("the mist"),
+        UPPER_MINES("upper mines"),
+        DWARVEN_BASE_CAMP("dwarven base camp"),
+        FOSSIL_RESEARCH_CENTER("fossil research center"),
+        GLACITE_TUNNELS("glacite tunnels"),
+        GREAT_GLACITE_LAKE("great glacite lake"),
+        GLACITE_MINESHAFTS("glacite mineshafts");
+
+        private final String name;
+        DwarvenMinesZones(String name) {
+            this.name = name;
+        }
+    }
+    public enum CrystalHollowsZones {
+        CRYSTAL_HOLLOWS("crystal hollows"),
+        CRYSTAL_NUCLEUS("crystal nucleus"),
+        GOBLIN_HOLDOUT("goblin holdout"),
+        GOBLIN_QUEEN_DEN("goblin queen's den"),
+        JUNGLE("jungle"),
+        JUNGLE_TEMPLE("jungle temple"),
+        PRECURSOR_REMNANTS("precursor remnants"),
+        LOST_PRECURSOR_CITY("lost precursor city"),
+        MITHRIL_DEPOSITS("mithril deposits"),
+        DRAGON_LAIR("dragon's lair"),
+        MINES_OF_DIVAN("mines of divan"),
+        MAGMA_FIELDS("magma fields"),
+        KHAZAD_DUM("khazad-dum"),
+        FAIRY_GROTTO("fairy grotto");
+
+        private final String name;
+        CrystalHollowsZones(String name) {
+            this.name = name;
+        }
+    }
+    public enum JerryIslandZones {
+        JERRY_WORKSHOP("jerry's workshop"),
+        JERRY_POND("jerry pond"),
+        REFLECTIVE_POND("reflective pond"),
+        SUNKEN_JERRY_POND("sunken jerry pond"),
+        MOUNT_JERRY("mount jerry"),
+        GARY_SHACK("gary's shack"),
+        GLACIAL_CAVE("glacial cave"),
+        TERRY_SHACK("terry's shack"),
+        HOT_SPRINGS("hot springs"),
+        SHERRY_SHOWROOM("sherry's showroom"),
+        EINARY_EMPORIUM("einary's emporium");
+
+        private final String name;
+        JerryIslandZones(String name) {
+            this.name = name;
+        }
+    }
+    public enum DungeonHubZones {
+        DUNGEON_HUB("dungeon hub");
+
+        private final String name;
+        DungeonHubZones(String name) {
+            this.name = name;
+        }
+    }
+    public enum RiftZones {
+        THE_RIFT("the rift"),
+        WIZARD_TOWER("wizard tower"),
+        WYLD_WOODS("wyld woods"),
+        ENIGMA_CRIB("enigma's crib"),
+        BROKEN_CAGE("broken cage"),
+        SHIFTED_TAVERN("shifted tavern"),
+        PUMPGROTTO("pumpgrotto"),
+        THE_BASTION("the bastion"),
+        OTHERSIDE("otherside"),
+        BLACK_LAGOON("black lagoon"),
+        LAGOON_CAVE("lagoon cave"),
+        LAGOON_HUT("lagoon hut"),
+        LEECHES_LAIR("leeches lair"),
+        AROUND_COLOSSEUM("around colosseum"),
+        RIFT_GALLERY_ENTRANCE("rift gallery entrance"),
+        RIFT_GALLERY("rift gallery"),
+        WEST_VILLAGE("west village"),
+        DOLPHIN_TRAINER("dolphin trainer"),
+        CAKE_HOUSE("cake house"),
+        INFESTED_HOUSE("infested house"),
+        MIRRORVERSE("mirrorverse"),
+        DREADFARM("dreadfarm"),
+        GREAT_BEANSTALK("great beanstalk"),
+        VILLAGE_PLAZA("village plaza"),
+        TAYLORS("taylor's"),
+        LONELY_TERRACE("lonely terrace"),
+        MURDER_HOUSE("murder house"),
+        BOOK_IN_A_BOOK("book in a book"),
+        HALF_EATEN_CAVE("half-eaten cave"),
+        YOUR_ISLAND("'your' island"),
+        EMPTY_BANK("empty bank"),
+        BARRY_CENTER("barry center"),
+        BARRY_HQ("barry hq"),
+        DEJA_VU_ALLEY("deja vu alley"),
+        LIVING_CAVE("living cave"),
+        LIVING_STILLNESS("living stillness"),
+        COLOSSEUM("colosseum"),
+        BARRIER_STREET("barrier street"),
+        PHOTON_PATHWAY("photon pathway"),
+        STILLGORE_CHATEAU("stillgore chateau"),
+        OUBLIETTE("oubliette"),
+        FAIRYLOSOPHER_TOWER("fairylosopher tower"),
+        MOUNTAINTOP("mountaintop"),
+        CONTINUUM("continuum"),
+        TIME_CHAMBER("time chamber"),
+        TIME_TORN_ISLES("time-torn isles"),
+        ROSE_END("rose's end"),
+        WALK_OF_FAME("walk of fame"),
+        CEREBRAL_CITADEL("cerebral citadel"),
+        WIZARDMAN_BUREAU("wizardman bureau"),
+        WIZARD_BRAWL("wizard brawl"),
+        TRIAL_GROUNDS("trial grounds");
+
+        private final String name;
+        RiftZones(String name) {
+            this.name = name;
+        }
+    }
+    public enum BackwaterBayouZones {
+        BACKWATER_BAYOU("backwater bayou");
+
+        private final String name;
+        BackwaterBayouZones(String name) {
+            this.name = name;
+        }
+    }
+
 }
