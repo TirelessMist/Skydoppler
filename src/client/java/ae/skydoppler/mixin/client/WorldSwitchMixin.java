6ba2ea14d7f2f3d72cd60d8e90efa9b123aa842f
TirelessMist <emardag@icloud.com>
2025-05-08 06:55:49 -0500

    Did nothing.

--- src/client/java/ae/skydoppler/mixin/client/WorldSwitchMixin.java
+++ src/client/java/ae/skydoppler/mixin/client/WorldSwitchMixin.java
@@ -1,17 +1,10 @@
 package ae.skydoppler.mixin.client;
 
 import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.world.ClientWorld;
-import net.minecraft.entity.Entity;
-import net.minecraft.world.ChunkLoadingManager;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
 public class WorldSwitchMixin {
-    
+
 
 }
