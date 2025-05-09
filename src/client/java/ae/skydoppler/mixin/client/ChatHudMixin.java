30f27022f104068c98c9889a8b39bff55df04640
TirelessMist <emardag@icloud.com>
2025-05-08 20:13:55 -0500

    Finished dealing with "\\hide" return value to hide chat message, but also sending a new chat message (client-side) containing anything that came after the "\\hide" part.

--- src/client/java/ae/skydoppler/mixin/client/ChatHudMixin.java
+++ src/client/java/ae/skydoppler/mixin/client/ChatHudMixin.java
@@ -1,10 +1,12 @@
 package ae.skydoppler.mixin.client;
 
 import ae.skydoppler.chat.ChatMatchHandler;
+import ae.skydoppler.chat.StyleConverter;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.ChatHud;
-import net.minecraft.client.particle.FishingParticle;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -12,29 +14,42 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ChatHud.class)
 public class ChatHudMixin {
 
+    @Unique
     MinecraftClient client = MinecraftClient.getInstance();
 
-    @Inject(at = @At("HEAD"), method = "addMessage*")
+    @Inject(at = @At("HEAD"), method = "addMessage*", cancellable = true)
     private void onAddMessage(Text chatMessage, CallbackInfo ci) {
 
-        chatMessage = StyleConverter.ConvertToString(chatMessage);
+        String messageString = StyleConverter.ConvertToFormattedString(chatMessage);
 
         System.out.println("[ChatHudMixin] -> onAddMessage -> chatMessage = \"" + chatMessage + "\"");
-        
-        String returnValue = ChatMatchHandler.checkForMatches(chatMessage);
 
-        if (returnValue.equalsIgnoreCase("\\hide")) {
+        String returnValue = ChatMatchHandler.checkForMatches(messageString);
+
+        if (returnValue.startsWith("\\hide")) {
+
+            if (returnValue.length() > 5) {
+
+                returnValue = returnValue.substring(5);
+
+            } else {
+
+                returnValue = "";
+
+            }
 
             ci.cancel();
-            return;
 
         }
 
-        if (!returnValue.empty()) {
+        if (!returnValue.isEmpty()) {
 
-            ci.cancel();
-            client.player.sendMessage(Text.literal(returnValue), false);
-            return;
+            if (client.player != null) {
+
+                client.player.sendMessage(Text.literal(returnValue), false);
+                return;
+
+            }
 
         }
 
