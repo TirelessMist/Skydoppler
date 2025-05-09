b78e1c5025541049b6d8318d34d4c0a762b46d36
TirelessMist <emardag@icloud.com>
2025-05-08 20:22:54 -0500

    This is currently useless. Removed all functions, and this file is only left here to rot.

---
+++ src/client/java/ae/skydoppler/mixin/client/WorldRendererMixin.java
@@ -0,0 +1,22 @@
+package ae.skydoppler.mixin.client;
+
+import ae.skydoppler.SkydopplerClient;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.EntityList;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import javax.xml.stream.events.EntityDeclaration;
+
+@Mixin(WorldRenderer.class)
+public class WorldRendererMixin {
+
+
+
+}
