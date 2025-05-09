5cd7a927b3ab50b0b1335e507cfca2ca270ff8ab
TirelessMist <emardag@icloud.com>
2025-05-08 20:17:13 -0500

    Added the method isPlayerAnNpc that returns true if the input PlayerEntity is an NPC (Real players use UUID ver. 4, while NPCs only use other versions).

--- src/client/java/ae/skydoppler/player_hiding/HidePlayerNearNpc.java
+++ src/client/java/ae/skydoppler/player_hiding/HidePlayerNearNpc.java
@@ -1,5 +1,15 @@
 package ae.skydoppler.player_hiding;
 
+import net.minecraft.entity.player.PlayerEntity;
+
 public class HidePlayerNearNpc {
-    public static boolean hidePlayers = false; // the setting - if it is enabled or not
+    public static boolean hidePlayers = true; // the setting - if it is enabled or not
+
+    public static double hideRange = 2; // the range in which players are hidden
+
+    public static boolean isPlayerAnNpc(PlayerEntity player) {
+
+        return player.getUuid().version() != 4;
+
+    }
 }
