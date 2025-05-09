0660cb77ab9d6e68fb0402552483afe8052cc219
VGRADN <vgradn@hsd.ca>
2025-05-09 12:17:34 -0500

    Added EntityFireHideState.java.

---
+++ src/client/java/ae/skydoppler/model/EntityFireHideState.java
@@ -0,0 +1,9 @@
+package ae.skydoppler.model;
+
+public class EntityFireHideState {
+    
+    public static boolean HideFireOnEntities = true;
+
+    public static boolean HideFirstPersonFire = true;
+    
+}
