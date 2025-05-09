be2b05f3723e6e5e057e93b4adae8fc382361f15
TirelessMist <emardag@icloud.com>
2025-05-09 07:03:53 -0500

    The ClientWorldMixin is currently useless. Removed all functions, and this file is only left here to rot.

---
+++ src/client/java/ae/skydoppler/mixin/client/ClientWorldMixin.java
@@ -0,0 +1,18 @@
+package ae.skydoppler.mixin.client;
+
+import net.minecraft.client.render.*;
+import net.minecraft.client.util.ObjectAllocator;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.math.Vec3d;
+import org.joml.Matrix4f;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientWorld.class)
+public class ClientWorldMixin {
+
+
+
+}
