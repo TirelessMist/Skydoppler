1ffee37cea23cb6143e9cb63a6943518fed987cd
TirelessMist <emardag@icloud.com>
2025-05-08 20:18:16 -0500

    Added some other variables so that the Legendary Seacreature Alert feature will be more customizable in the future.

---
+++ src/client/java/ae/skydoppler/fishing/SeacreatureMessageState.java
@@ -0,0 +1,10 @@
+package ae.skydoppler.fishing;
+
+public class SeacreatureMessageState {
+    public static boolean doLegSeacreatureAlerts = true;
+
+    public static boolean shouldHideOriginalMessage = true;
+    public static boolean showCustomChatMessage = true;
+    public static boolean showTitle = true;
+    public static boolean shouldPlaySound = true;
+}
