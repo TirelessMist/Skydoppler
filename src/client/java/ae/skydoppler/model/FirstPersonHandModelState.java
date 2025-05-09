c311836678bcd1688f5affdd2e27c662604cfc11
VGRADN <vgradn@hsd.ca>
2025-05-09 12:17:45 -0500

    Added FirstPersonHandModelState.java.

---
+++ src/client/java/ae/skydoppler/model/FirstPersonHandModelState.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package ae.skydoppler.model;
+
+/**
+ *
+ * @author VGRADN
+ */
+public class FirstPersonHandModelState {
+
+    public static float dX = 0;
+    public static float dY = 0;
+    public static float dZ = 0;
+
+    public static float dPitch = 0;
+    public static float dYaw = 0;
+    public static float dRoll = 0;
+
+    public static float scaleSize = 1.0f;
+    
+    public static float scaleSwingSpeed = 1.0f;
+    public static boolean ignoreHaste = false;
+
+}
