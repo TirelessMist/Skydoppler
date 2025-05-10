9b46551f67fff8cca342e3c7db207207fa96dbe6
TirelessMist <tirelessmist@outlook.com>
2025-05-10 08:51:58 -0500

    Renamed SkyblockIslandType.java to SkyblockIslandCategoryEnum.java for better descriptiveness. Also added SPECIAL_ISLAND type for islands like Jerry Island (and maybe more in future Skyblock updates).

--- src/client/java/ae/skydoppler/skyblock_locations/SkyblockIslandType.java
+++
@@ -1,5 +0,0 @@
-package ae.skydoppler.skyblock_locations;
-
-public enum SkyblockIslandType {
-    NONE, HUB, PRIVATE_ISLAND, MINING_ISLAND, FARMING_ISLAND, FORAGING_ISLAND, FISHING_ISLAND, COMBAT_ISLAND, DUNGEON_ISLAND, KUUDRA_ISLAND, RIFT_ISLAND
-}
