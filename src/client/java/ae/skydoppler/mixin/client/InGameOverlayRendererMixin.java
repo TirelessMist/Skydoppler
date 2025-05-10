4eb88351f7ec82d4a062cf35e0f8542b97d80c70
TirelessMist <tirelessmist@outlook.com>
2025-05-10 10:11:36 -0500

    Created InGameOverlayRendererMixin.java to handle the rendering (and cancelling the rendering of) screen overlays such as the first-person fire and underwater overlays.

---
+++ src/client/java/ae/skydoppler/mixin/client/InGameOverlayRendererMixin.java
@@ -0,0 +1,37 @@
+package ae.skydoppler.mixin.client;
+
+import ae.skydoppler.model.EntityFireHideState;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.InGameOverlayRenderer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.texture.Sprite;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameOverlayRenderer.class)
+public class InGameOverlayRendererMixin {
+
+    @Inject(method = "renderFireOverlay", at = @At("HEAD"), cancellable = true)
+    private static void onSetupOverlayColor(MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo ci) {
+        if (EntityFireHideState.HideFirstPersonFire) {
+
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "renderUnderwaterOverlay", at = @At("HEAD"), cancellable = true)
+    private static void onRenderUnderwaterOverlay(MinecraftClient client, MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo ci) {
+
+        ci.cancel();
+    }
+
+    @Inject(method = "renderInWallOverlay", at = @At("HEAD"), cancellable = true)
+    private static void onRenderInWallOverlay(Sprite sprite, MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo ci) {
+
+        ci.cancel();
+    }
+
+}
