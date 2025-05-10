38e2319911e42916f40723326c6e9c1a37e193d9
TirelessMist <tirelessmist@outlook.com>
2025-05-10 08:48:50 -0500

    Created InGameHudMixin and added Injection functions to hide vanilla HUD elements, depending on the boolean config settings variables inside of HideVanillaHudElementsState.java.

---
+++ src/client/java/ae/skydoppler/mixin/client/InGameHudMixin.java
@@ -0,0 +1,78 @@
+package ae.skydoppler.mixin.client;
+
+import ae.skydoppler.hud.HideVanillaHudElementsState;
+import ae.skydoppler.model.EntityFireHideState;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public class InGameHudMixin {
+
+    @Inject(method = "renderStatusEffectOverlay", at = @At("HEAD"), cancellable = true)
+    private void onRenderStatusEffectOverlay(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
+
+        if (HideVanillaHudElementsState.shouldHideStatusEffectOverlay)
+            ci.cancel();
+    }
+
+    @Inject(method = "renderFood", at = @At("HEAD"), cancellable = true)
+    private void onRenderFood(DrawContext context, PlayerEntity player, int top, int right, CallbackInfo ci) {
+
+        if (HideVanillaHudElementsState.shouldHideHungerArmorBubbles)
+            ci.cancel();
+    }
+
+    @Inject(method = "renderArmor", at = @At("HEAD"), cancellable = true)
+    private static void onRenderArmor(DrawContext context, PlayerEntity player, int i, int j, int k, int x, CallbackInfo ci) {
+
+        if (HideVanillaHudElementsState.shouldHideHungerArmorBubbles)
+            ci.cancel();
+    }
+
+    @Inject(method = "renderAirBubbles", at = @At("HEAD"), cancellable = true)
+    private void onRenderAirBubbles(DrawContext context, PlayerEntity player, int heartCount, int top, int left, CallbackInfo ci) {
+
+        if (HideVanillaHudElementsState.shouldHideHungerArmorBubbles)
+            ci.cancel();
+    }
+
+    @Inject(method = "renderHeldItemTooltip", at = @At("HEAD"), cancellable = true)
+    private void onRenderHeldItemTooltip(DrawContext context, CallbackInfo ci) {
+
+        if (HideVanillaHudElementsState.shouldHideHeldItemTooltip)
+            ci.cancel();
+    }
+
+    @Inject(method = "renderHealthBar", at = @At("HEAD"), cancellable = true)
+    private void onRenderHealthBar(DrawContext context, PlayerEntity player, int x, int y, int lines, int regeneratingHeartIndex, float maxHealth, int lastHealth, int health, int absorption, boolean blinking, CallbackInfo ci) {
+
+        if (HideVanillaHudElementsState.shouldHideHealthBar)
+            ci.cancel();
+    }
+
+    @Inject(method = "renderMountHealth", at = @At("HEAD"), cancellable = true)
+    private void onRenderMountHealth(DrawContext context, CallbackInfo ci) {
+
+        if (HideVanillaHudElementsState.shouldHideHealthBar)
+            ci.cancel();
+    }
+
+    @Inject(method = "renderOverlay", at = @At("HEAD"), cancellable = true)
+    private void onRenderOverlay(DrawContext context, Identifier texture, float opacity, CallbackInfo ci) {
+
+        if (EntityFireHideState.HideFirstPersonFire && texture.getPath().contains("fire_layer_")) {
+            // Cancel the overlay render if the overlay is the first-person fire overlay.
+
+            ci.cancel();
+        }
+
+    }
+
+}
