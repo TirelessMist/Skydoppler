79dd89af66ac2cae04188022a69ad9191c1b9910
TirelessMist <tirelessmist@outlook.com>
2025-05-10 08:46:37 -0500

    Changed the static function isPlayerAnNpc to take in a generic Entity instead of the PlayerEntity (realized that you could get the uuid from a plain Entity; also doesn't require a cast to PlayerEntity from Entity).

--- src/client/java/ae/skydoppler/player_hiding/HidePlayerNearNpc.java
+++ src/client/java/ae/skydoppler/player_hiding/HidePlayerNearNpc.java
@@ -1,13 +1,13 @@
 package ae.skydoppler.player_hiding;
 
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.Entity;
 
 public class HidePlayerNearNpc {
     public static boolean hidePlayers = true; // the setting - if it is enabled or not
 
     public static double hideRange = 2; // the range in which players are hidden
 
-    public static boolean isPlayerAnNpc(PlayerEntity player) {
+    public static boolean isPlayerAnNpc(Entity player) {
 
         return player.getUuid().version() != 4;
 
