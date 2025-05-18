b3bb92364b3d2d7ebd78779e30ebb92dcaf54ab2
TirelessMist <emardag@icloud.com>
2025-05-17 19:59:28 -0500

    Added specific mount health boolean (previously combined with normal hearts). Also added new boolean for hiding the portal overlay.

--- src/client/java/ae/skydoppler/hud/HideVanillaHudElementsState.java
+++ src/client/java/ae/skydoppler/hud/HideVanillaHudElementsState.java
@@ -6,5 +6,7 @@ public class HideVanillaHudElementsState {
     public static boolean shouldHideHealthBar = false;
     public static boolean shouldHideStatusEffectOverlay = true;
     public static boolean shouldHideHeldItemTooltip = false;
+    public static boolean shouldHideMountHealth = true;
+    public static boolean shouldHidePortalOverlay = true;
 
 }
