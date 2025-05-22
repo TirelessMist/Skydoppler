3c592638e4cf3c3f2a1f014e1daae37ea95e5f98
TirelessMist <emardag@icloud.com>
2025-05-22 16:23:28 -0500

    Removed EntityFireHideState.java as its variables were integrated into the config.

--- src/client/java/ae/skydoppler/model/EntityFireHideState.java
+++
@@ -1,9 +0,0 @@
-package ae.skydoppler.model;
-
-public class EntityFireHideState {
-    
-    public static boolean HideFireOnEntities = true;
-
-    public static boolean HideFirstPersonFire = true;
-    
-}
