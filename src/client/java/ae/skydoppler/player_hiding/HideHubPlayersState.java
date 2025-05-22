d50232bbc2f0dfd622b8a0b7286caa389febed7f
TirelessMist <emardag@icloud.com>
2025-05-22 06:28:44 -0500

    Integrated HideHubPlayersState.java with the config.

--- src/client/java/ae/skydoppler/player_hiding/HideHubPlayersState.java
+++ src/client/java/ae/skydoppler/player_hiding/HideHubPlayersState.java
@@ -4,22 +4,12 @@ import ae.skydoppler.SkydopplerClient;
 import ae.skydoppler.skyblock_locations.SkyblockIslandEnum;
 
 public class HideHubPlayersState {
-    public enum HideLocationMode {
-        NONE, VILLAGE_ONLY, HUB_ENTIRE, DUNGEON_AND_NORMAL_HUB, DUNGEON_AND_VILLAGE;
-
-        private final String translationKey;
-
-        HideLocationMode() {
-            this.translationKey = "config.ae.skydoppler.option." + this.name().toLowerCase();
-        }
-
-        public String getTranslationKey() {
-            return translationKey;
-        }
-    }
     public static int showRange = 12 /*0.1 * MinecraftClient.getInstance().worldRenderer.getViewDistance()*/;
 
     public static boolean shouldHidePlayers() {
+
+        if (!SkydopplerClient.CONFIG.doFarPlayerHiding) return false;
+
         switch (SkydopplerClient.CONFIG.hideFarPlayersMode) {
             case HideHubPlayersState.HideLocationMode.HUB_ENTIRE -> {
                 return SkydopplerClient.currentIsland == SkyblockIslandEnum.HUB;
@@ -35,6 +25,20 @@ public class HideHubPlayersState {
             }
         }
         return false;
+    }
+
+    public enum HideLocationMode {
+        VILLAGE_ONLY, HUB_ENTIRE, DUNGEON_AND_NORMAL_HUB, DUNGEON_AND_VILLAGE;
+
+        private final String translationKey;
+
+        HideLocationMode() {
+            this.translationKey = "config.ae.skydoppler.option." + this.name().toLowerCase();
+        }
+
+        public String getTranslationKey() {
+            return translationKey;
+        }
     }
 
 }
