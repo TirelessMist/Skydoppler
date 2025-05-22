38c03bc970aadb9bda505ac8f62e4106879f9e52
TirelessMist <emardag@icloud.com>
2025-05-22 16:28:06 -0500

    Integrated BossBarHudMixin.java with the config.

--- src/client/java/ae/skydoppler/mixin/client/BossBarHudMixin.java
+++ src/client/java/ae/skydoppler/mixin/client/BossBarHudMixin.java
@@ -1,24 +1,14 @@
 package ae.skydoppler.mixin.client;
 
-import ae.skydoppler.hud.BossbarHideState;
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import ae.skydoppler.SkydopplerClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.BossBarHud;
-import net.minecraft.client.gui.hud.ClientBossBar;
 import net.minecraft.entity.boss.BossBar;
-import net.minecraft.entity.boss.BossBarManager;
-import net.minecraft.entity.boss.CommandBossBar;
-import net.minecraft.entity.boss.WitherEntity;
-import net.minecraft.network.packet.s2c.play.BossBarS2CPacket;
 import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.UUID;
 
 @Mixin(BossBarHud.class)
 public class BossBarHudMixin {
@@ -26,7 +16,7 @@ public class BossBarHudMixin {
     @Inject(method = "renderBossBar(Lnet/minecraft/client/gui/DrawContext;IILnet/minecraft/entity/boss/BossBar;)V", at = @At("HEAD"), cancellable = true)
     private void onRenderBossBar(DrawContext context, int x, int y, BossBar bossBar, CallbackInfo ci) {
 
-        if (BossbarHideState.shouldHideWitherBossbars && bossBar.shouldDarkenSky()) {
+        if (SkydopplerClient.CONFIG.vanillaHudConfig.shouldHideWitherBossbarsInSkyblock && bossBar.shouldDarkenSky()) {
             bossBar.setName(Text.empty());
             ci.cancel();
         }
