33138db31e23eb963a4902a8594f7a3622ca86c3
TirelessMist <emardag@icloud.com>
2025-05-31 15:26:49 -0500

    Added Skydoppler config commands

--- src/client/java/ae/skydoppler/SkydopplerClient.java
+++ src/client/java/ae/skydoppler/SkydopplerClient.java
@@ -1,11 +1,14 @@
 package ae.skydoppler;
 
 import ae.skydoppler.chat.ChatMatchHandler;
+import ae.skydoppler.command.SkydopplerCommandHandler;
 import ae.skydoppler.config.SkydopplerConfig;
+import ae.skydoppler.config.held_item_config.HeldItemConfigScreen;
 import ae.skydoppler.dungeon.DungeonClientHandler;
 import ae.skydoppler.skyblock_locations.SkyblockLocationEnum;
 import ae.skydoppler.structs.SkyblockPlayerDataStruct;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
@@ -20,27 +23,20 @@ import java.nio.file.Paths;
 
 public class SkydopplerClient implements ClientModInitializer {
 
+    public static final Path CONFIG_PATH = Paths.get(MinecraftClient.getInstance() // or use a proper run directory reference
+            .runDirectory.getAbsolutePath(), "config", "skydoppler.json");
     public static MinecraftClient client = MinecraftClient.getInstance();
-
     public static KeyBinding debugKey;
-
     public static Boolean debugModeEnabled = true;
-
     public static SkyblockPlayerDataStruct playerDataStruct;
     public static DungeonClientHandler dungeonClientHandler;
-
     public static SkyblockLocationEnum currentIsland = SkyblockLocationEnum.NONE;
     public static Enum<?> currentZone = SkyblockLocationEnum.NONE.getZonesForIsland()[0];
     public static Enum<?> currentRegion = null;
-
     public static boolean isRodCast;
     public static boolean isPlayingSkyblock = false;
-    
-    private TextRenderer textRenderer;
-
     public static SkydopplerConfig CONFIG;
-    public static final Path CONFIG_PATH = Paths.get(MinecraftClient.getInstance() // or use a proper run directory reference
-            .runDirectory.getAbsolutePath(), "config", "skydoppler.json");
+    private TextRenderer textRenderer;
 
     @Override
     public void onInitializeClient() {
@@ -51,6 +47,11 @@ public class SkydopplerClient implements ClientModInitializer {
         // Load configuration on initialization
         CONFIG = SkydopplerConfig.load(CONFIG_PATH);
 
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+
+            SkydopplerCommandHandler.registerCommands(dispatcher);
+
+        });
 
         textRenderer = new TextRenderer(client);
         textRenderer.initialize();
@@ -69,8 +70,7 @@ public class SkydopplerClient implements ClientModInitializer {
             if (client.player == null || client.world == null) return;
 
             while (debugKey.wasPressed()) {
-                // Open the Held Item Config screen when debug key is pressed
-                client.setScreen(new ae.skydoppler.config.held_item_config.HeldItemConfigScreen(null));
+                client.setScreen(new HeldItemConfigScreen(null));
             }
 
             if (client.world == null || client.player == null) {
