390b4af4a57fba0e1938483f79aa518e3a9572bc
TirelessMist <emardag@icloud.com>
2025-06-01 07:24:32 -0500

    Put the config screen rendering calls on the client thread

--- src/client/java/ae/skydoppler/command/SkydopplerCommand.java
+++ src/client/java/ae/skydoppler/command/SkydopplerCommand.java
@@ -1,42 +1,44 @@
 package ae.skydoppler.command;
 
+import ae.skydoppler.config.ConfigScreenHandler;
 import ae.skydoppler.config.held_item_config.HeldItemConfigScreen;
 import com.mojang.brigadier.CommandDispatcher;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.client.MinecraftClient;
+import ae.skydoppler.SkydopplerClient;
 
-@Environment(EnvType.CLIENT)
 public class SkydopplerCommand {
     public static void register() {
-        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
             registerCommands(dispatcher);
         });
     }
 
     private static void registerCommands(CommandDispatcher<FabricClientCommandSource> dispatcher) {
         dispatcher.register(ClientCommandManager.literal("skydoppler")
-                .then(ClientCommandManager.literal("config")
-                        .executes(ctx -> {
-                            MinecraftClient.getInstance().setScreen(new HeldItemConfigScreen(MinecraftClient.getInstance().currentScreen));
-                            return 1;
-                        })
-                        .then(ClientCommandManager.literal("HeldItemRenderConfig")
-                                .executes(ctx -> {
-                                    MinecraftClient.getInstance().setScreen(new HeldItemConfigScreen(MinecraftClient.getInstance().currentScreen));
-                                    return 1;
-                                })
-                        )
-                )
+            .then(ClientCommandManager.literal("config")
+                .executes(ctx -> {
+                    MinecraftClient client = MinecraftClient.getInstance();
+                    client.execute(() -> client.setScreen(ConfigScreenHandler.buildConfigScreen(SkydopplerClient.CONFIG)));
+                    return 1;
+                })
                 .then(ClientCommandManager.literal("HeldItemRenderConfig")
-                        .executes(ctx -> {
-                            MinecraftClient.getInstance().setScreen(new HeldItemConfigScreen(MinecraftClient.getInstance().currentScreen));
-                            return 1;
-                        })
+                    .executes(ctx -> {
+                        MinecraftClient client = MinecraftClient.getInstance();
+                        client.execute(() -> client.setScreen(new HeldItemConfigScreen(MinecraftClient.getInstance().currentScreen)));
+                        return 1;
+                    })
                 )
+            )
+            .then(ClientCommandManager.literal("HeldItemRenderConfig")
+                .executes(ctx -> {
+                    MinecraftClient client = MinecraftClient.getInstance();
+                    client.execute(() -> client.setScreen(new HeldItemConfigScreen(MinecraftClient.getInstance().currentScreen)));
+                    return 1;
+                })
+            )
         );
     }
 }
