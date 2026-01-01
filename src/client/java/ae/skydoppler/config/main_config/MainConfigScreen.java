bdcb47db582b3e444ef4d9b777fd5d653409623b
TirelessMist <emardag@icloud.com>
2026-01-01 14:45:53 -0600

    Fixed z-ordering

--- src/client/java/ae/skydoppler/config/main_config/MainConfigScreen.java
+++ src/client/java/ae/skydoppler/config/main_config/MainConfigScreen.java
@@ -745,6 +745,7 @@ public class MainConfigScreen extends Screen {
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         this.renderBackground(context, mouseX, mouseY, delta);
+        super.render(context, mouseX, mouseY, delta);
 
         // Draw title
         context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 20, 0xFFFFFF);
@@ -757,7 +758,6 @@ public class MainConfigScreen extends Screen {
         // Draw scrollbar
         drawScrollbar(context);
 
-        super.render(context, mouseX, mouseY, delta);
 
         // Draw config entries
         drawConfigEntries(context);
